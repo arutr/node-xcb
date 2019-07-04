505cc46c44622fc72453f10e77073e69a346a643
Artur Komoter <artur@komoter.pl>
2019-07-04 22:24:51 +0100

    Removed stub files

--- lib/composite.cc
+++
@@ -1,8 +0,0 @@
-// xcb_composite_redirect_t
-// xcb_composite_query_version_cookie_t
-// xcb_composite_query_version_reply_t
-
-// xcb_composite_name_window_pixmap
-// xcb_composite_redirect_subwindows
-// xcb_composite_query_version
-// xcb_composite_query_version_reply
--- lib/event.cc
+++
@@ -1,2 +0,0 @@
-// xcb_event_get_error_label
-// xcb_event_get_label
