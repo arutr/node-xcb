338ca176756fd73100039a9796e29b0e811683d8
Artur Komoter <artur@komoter.pl>
2019-07-18 15:22:33 +0100

    Removed padding members from objects

--- lib/core.cc
+++ lib/core.cc
@@ -10,8 +10,8 @@ Napi::Object CreateGenericErrorObject(const Napi::CallbackInfo& info, const xcb_
 	object["resourceId"] = Napi::Number::New(env, generic_error->resource_id);
 	object["minorCode"] = Napi::Number::New(env, generic_error->minor_code);
 	object["majorCode"] = Napi::Number::New(env, generic_error->major_code);
-	object["pad0"] = Napi::Number::New(env, generic_error->pad0);
-	object["pad"] = Napi::ArrayBuffer::New(env, (void*) generic_error->pad, sizeof(uint32_t) * 5);
+	// object["pad0"] = Napi::Number::New(env, generic_error->pad0);
+	// object["pad"] = Napi::ArrayBuffer::New(env, (void*) generic_error->pad, sizeof(uint32_t) * 5);
 	object["fullSequence"] = Napi::Number::New(env, generic_error->full_sequence);
 
 	return object;
@@ -22,9 +22,9 @@ Napi::Object Core::CreateGenericEventObject(const Napi::CallbackInfo& info, cons
 	auto env = info.Env();
 	auto object = Napi::Object::New(env);
 	object["responseType"] = Napi::Number::New(env, generic_event->response_type);
-	object["pad0"] = Napi::Number::New(env, generic_event->pad0);
+	// object["pad0"] = Napi::Number::New(env, generic_event->pad0);
 	object["sequence"] = Napi::Number::New(env, generic_event->sequence);
-	object["pad"] = Napi::ArrayBuffer::New(env, (void*) generic_event->pad, sizeof(uint32_t) * 7);
+	// object["pad"] = Napi::ArrayBuffer::New(env, (void*) generic_event->pad, sizeof(uint32_t) * 7);
 	object["fullSequence"] = Napi::Number::New(env, generic_event->full_sequence);
 
 	return object;
@@ -35,7 +35,7 @@ Napi::Object Core::CreateGenericReplyObject(const Napi::CallbackInfo& info, cons
 	auto env = info.Env();
 	auto object = Napi::Object::New(env);
 	object["responseType"] = Napi::Number::New(env, generic_reply->response_type);
-	object["pad0"] = Napi::Number::New(env, generic_reply->pad0);
+	// object["pad0"] = Napi::Number::New(env, generic_reply->pad0);
 	object["sequence"] = Napi::Number::New(env, generic_reply->sequence);
 	object["length"] = Napi::Number::New(env, generic_reply->length);
 
